b58b575f9bbc8dca13564aac1e2ff95072580789
Muhamad Iqbal Nurzaman <iqbal.nurzaman@andalin.com>
2023-08-25 20:02:03 +0700

    Clean up auth controller

--- src/app/controllers/auth.controller.ts
+++ src/app/controllers/auth.controller.ts
@@ -1,22 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-
 import authService from "../services/auth.service";
-import { UserCreationAttributes } from "../../models/interfaces/user.interface";
-
-export async function register(req: Request, res: Response, next: NextFunction): Promise<void> {
-    const payload = req.body as UserCreationAttributes
-    try {
-        const user = await authService.signUp(payload)
-
-        res.status(201).json({
-            message: 'Successfully Registered!',
-            code: 201,
-            data: user
-        })
-    } catch (err) {
-        next(err)
-    }
-}
 
 export async function login(req: Request, res: Response, next: NextFunction): Promise<void> {
     const { email, password } = req.body
